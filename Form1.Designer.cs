2e73bce5634ece625062685abe5cbc7ce251bd26
cniks2503 <cniksofficial@gmail.com>
2021-05-29 15:52:44 +0530

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.dgvUser = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtKnowledge = new System.Windows.Forms.TextBox();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHobbies = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnSave.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSave.Location = new System.Drawing.Point(76, 390);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(91, 36);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(124, 51);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(319, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // dgvUser
+            // 
+            this.dgvUser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUser.Location = new System.Drawing.Point(463, 145);
+            this.dgvUser.Name = "dgvUser";
+            this.dgvUser.Size = new System.Drawing.Size(411, 346);
+            this.dgvUser.TabIndex = 2;
+            this.dgvUser.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvUser_CellContentClick);
+            this.dgvUser.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUser_CellDoubleClick_1);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnDelete.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDelete.Location = new System.Drawing.Point(284, 390);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(91, 36);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtKnowledge
+            // 
+            this.txtKnowledge.Location = new System.Drawing.Point(228, 205);
+            this.txtKnowledge.Name = "txtKnowledge";
+            this.txtKnowledge.Size = new System.Drawing.Size(215, 20);
+            this.txtKnowledge.TabIndex = 6;
+            // 
+            // txtGender
+            // 
+            this.txtGender.Location = new System.Drawing.Point(124, 129);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(90, 20);
+            this.txtGender.TabIndex = 8;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnSearch.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSearch.Location = new System.Drawing.Point(625, 90);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(88, 36);
+            this.btnSearch.TabIndex = 7;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtContact
+            // 
+            this.txtContact.Location = new System.Drawing.Point(124, 90);
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(181, 20);
+            this.txtContact.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 19);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 19);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Contact";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 19);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(24, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(321, 19);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Are you familiar with C/C++,C#,VB,Delphi?";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(463, 51);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(411, 20);
+            this.txtSearch.TabIndex = 16;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 245);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 19);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Hobbies";
+            // 
+            // txtHobbies
+            // 
+            this.txtHobbies.Location = new System.Drawing.Point(124, 246);
+            this.txtHobbies.Multiline = true;
+            this.txtHobbies.Name = "txtHobbies";
+            this.txtHobbies.Size = new System.Drawing.Size(319, 123);
+            this.txtHobbies.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(24, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(198, 19);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Yes / No (Specify Which?)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(888, 510);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtHobbies);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtContact);
+            this.Controls.Add(this.txtGender);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtKnowledge);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.dgvUser);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnSave);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MyArbinAssign App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.DataGridView dgvUser;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtKnowledge;
+        private System.Windows.Forms.TextBox txtGender;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHobbies;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Server=localhost;Database=database;Uid=root;Pwd=Nikita@123";
+        int ID = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            GridFill();
+        }
+
+        void Clear()
+        {
+            txtName.Text = txtContact.Text = txtGender.Text = txtHobbies.Text = txtKnowledge.Text = "";
+            ID = 0;
+            btnSave.Text = "Save";
+        }
+
+        void GridFill()
+        {
+
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter("ViewAll", mysqlCon);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dtbluser_info = new DataTable();
+                sqlDa.Fill(dtbluser_info);
+                dgvUser.DataSource = dtbluser_info;
+                dgvUser.Columns[0].Visible = false;
+
+
+
+            }
+        }
+
+        private void DgvUser_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("AddEdit", mysqlCon);
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                mySqlCmd.Parameters.AddWithValue("_userID", ID);
+                mySqlCmd.Parameters.AddWithValue("_userName", txtName.Text.Trim());
+                mySqlCmd.Parameters.AddWithValue("_Contact", txtContact.Text.Trim());
+                mySqlCmd.Parameters.AddWithValue("_Gender", txtGender.Text.Trim());
+                mySqlCmd.Parameters.AddWithValue("_Knowledge", txtKnowledge.Text.Trim());
+                mySqlCmd.Parameters.AddWithValue("_Hobbies", txtHobbies.Text.Trim());
+                mySqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Added Successfully");
+                Clear();
+                GridFill();
+            }
+        }
+
+        private void dgvUser_CellDoubleClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvUser.CurrentRow.Index != -1)
+            {
+                txtName.Text = dgvUser.CurrentRow.Cells[1].Value.ToString();
+                txtContact.Text = dgvUser.CurrentRow.Cells[2].Value.ToString();
+                txtGender.Text = dgvUser.CurrentRow.Cells[3].Value.ToString();
+                txtKnowledge.Text = dgvUser.CurrentRow.Cells[4].Value.ToString();
+                txtHobbies.Text = dgvUser.CurrentRow.Cells[5].Value.ToString();
+                ID = Convert.ToInt32(dgvUser.CurrentRow.Cells[0].Value.ToString());
+                btnSave.Text = "Update";
+
+
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter("Search", mysqlCon);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                sqlDa.SelectCommand.Parameters.AddWithValue("_Value", txtSearch.Text);
+                DataTable dtbluser_info = new DataTable();
+                sqlDa.Fill(dtbluser_info);
+                dgvUser.DataSource = dtbluser_info;
+                dgvUser.Columns[0].Visible = false;
+                txtSearch.Text = "";
+
+
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("Delete", mysqlCon);
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                mySqlCmd.Parameters.AddWithValue("_userID", ID);
+                mySqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Deleted Successfully");
+                Clear();
+                GridFill();
+                btnSave.Text = "Save";
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
